90f7acb4c8f536a528469ca1bcb354c3ff4fa6bc
Victor Arturo Ruiz Cuervo <61213275+2dumb2program@users.noreply.github.com>
2020-07-20 19:07:29 -0500

    Correción de variables
    Arreglé algunas variables de mis controladores de FXML

--- src/main/java/com/spp/gui/controller/ControllerAddActivity.java
+++ src/main/java/com/spp/gui/controller/ControllerAddActivity.java
@@ -1,9 +1,11 @@
 package com.spp.gui.controller;
 
 
+import com.spp.model.domain.Activity;
+import com.spp.model.domain.Practitioner;
 import java.net.URL;
+import java.sql.Timestamp;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
@@ -16,28 +18,17 @@ import javafx.scene.control.TextField;
 
 public class ControllerAddActivity implements Initializable {
 
-    @FXML
-    private TextField textFieldTitle;
-    @FXML
-    private TextField textFieldEstimatedHours;
-    @FXML
-    private TextArea textAreaDescription;
-    @FXML
-    private DatePicker datePickerDueDate;
-    @FXML
-    private TableView<?> tableViewActivity;
-    @FXML
-    private TableColumn<?, ?> columnID;
-    @FXML
-    private TableColumn<?, ?> columnTitle;
-    @FXML
-    private TableColumn<?, ?> columnDueDate;
-    @FXML
-    private TableColumn<?, ?> columnEstimatedHours;
-    @FXML
-    private TableColumn<?, ?> columnPracticing;
-    @FXML
-    private ComboBox<?> comboBoxPractitioner;
+    @FXML private TextField textFieldTitle;
+    @FXML private TextField textFieldEstimatedHours;
+    @FXML private TextArea textAreaDescription;
+    @FXML private DatePicker datePickerDueDate;
+    @FXML private TableView<Activity> tableViewActivity;
+    @FXML private TableColumn<Activity, Integer> columnID;
+    @FXML private TableColumn<Activity, String> columnTitle;
+    @FXML private TableColumn<Activity, Timestamp> columnDueDate;
+    @FXML private TableColumn<Activity, Integer> columnEstimatedHours;
+    @FXML private TableColumn<Activity, Practitioner> columnPractitioner;
+    @FXML private ComboBox<Practitioner> comboBoxPractitioner;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -45,14 +36,14 @@ public class ControllerAddActivity implements Initializable {
     }    
 
     @FXML
-    private void generateActivity(ActionEvent event) {
+    private void generateActivity() {
     }
 
     @FXML
-    private void returnActivitySection(ActionEvent event) {
+    private void returnActivitySection() {
     }
 
     @FXML
-    private void showAllActivities(SortEvent<?> event) {
+    private void showAllActivities(SortEvent<Activity> event) {
     }
 }
--- src/main/java/com/spp/gui/controller/ControllerAddCompany.java
+++ src/main/java/com/spp/gui/controller/ControllerAddCompany.java
@@ -2,35 +2,23 @@ package com.spp.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
 public class ControllerAddCompany implements Initializable {
 
-    @FXML
-    private TextField textFieldName;
-    @FXML
-    private TextField textFieldAddress;
-    @FXML
-    private TextField textFieldState;
-    @FXML
-    private TextField textFieldCity;
-    @FXML
-    private TextField textFieldSector;
-    @FXML
-    private TextField textFieldPhone;
-    @FXML
-    private TextField textFieldEmail;
-    @FXML
-    private TextField textFieldSurnameResponisble;
-    @FXML
-    private TextField textFieldPhoneResponsible;
-    @FXML
-    private TextField textFieldEmailResponsible;
-    @FXML
-    private TextField textFieldNameResponsible;
+    @FXML private TextField textFieldName;
+    @FXML private TextField textFieldAddress;
+    @FXML private TextField textFieldState;
+    @FXML private TextField textFieldCity;
+    @FXML private TextField textFieldSector;
+    @FXML private TextField textFieldPhone;
+    @FXML private TextField textFieldEmail;
+    @FXML private TextField textFieldSurnameResponisble;
+    @FXML private TextField textFieldPhoneResponsible;
+    @FXML private TextField textFieldEmailResponsible;
+    @FXML private TextField textFieldNameResponsible;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -38,11 +26,10 @@ public class ControllerAddCompany implements Initializable {
     }    
 
     @FXML
-    private void registerCompany(ActionEvent event) {
+    private void registerCompany() {
     }
 
     @FXML
-    private void returnAddProject(ActionEvent event) {
+    private void returnAddProject() {
     }
-    
 }
