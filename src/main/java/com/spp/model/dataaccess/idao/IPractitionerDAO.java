43affd990cd0d9c02fff691954323d229649d30f
Abner Jeffrey Tapia Cruz <61332521+DrearyStudent@users.noreply.github.com>
2020-07-21 17:36:29 -0500

    fixes

--- src/main/java/com/spp/model/dataaccess/idao/IPractitionerDAO.java
+++ src/main/java/com/spp/model/dataaccess/idao/IPractitionerDAO.java
@@ -3,13 +3,9 @@ package com.spp.model.dataaccess.idao;
 import com.spp.model.domain.Group;
 import com.spp.model.domain.Practitioner;
 import javafx.collections.ObservableList;
-
-public interface IPractitionerDAO extends IUserDAO<Practitioner> {
-    void getPractitionerInformation(ObservableList<Practitioner> listPractitioner);
 import java.util.List;
 
 public interface IPractitionerDAO extends IUserDAO<Practitioner> {
-
+    void getPractitionerInformation(ObservableList<Practitioner> listPractitioner);
     List<Practitioner> getGroupStudents(Group group);
-
 }
