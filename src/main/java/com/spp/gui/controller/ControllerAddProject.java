fe51a17c8df54ffd0b91a5f9ee5166469d61aa9a
Victor Arturo Ruiz Cuervo <61213275+2dumb2program@users.noreply.github.com>
2020-07-21 16:22:16 -0500

    Funcionalidad de la ventana View_AddProject

--- src/main/java/com/spp/gui/controller/ControllerAddProject.java
+++ src/main/java/com/spp/gui/controller/ControllerAddProject.java
@@ -1,20 +1,34 @@
 package com.spp.gui.controller;
 
+import static com.spp.gui.Dialog.displayEmptyFields;
+import static com.spp.gui.Dialog.displayRecordConfirmation;
+import static com.spp.gui.Dialog.displayRecordSuccessDialog;
+import static com.spp.gui.Dialog.displaySomethingWentWrong;
+import com.spp.model.dataaccess.dao.ProjectDAO;
+import com.spp.model.dataaccess.idao.IProjectDAO;
 import com.spp.model.domain.Project;
-import com.spp.model.domain.ProjectResponsible;
 import com.spp.model.domain.RelatedCompany;
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.SortEvent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
-public class ControllerAddProject implements Initializable {
+public class ControllerAddProject {
 
     @FXML private TextField textFieldTitle;
     @FXML private TextArea textAreaDescription;
@@ -24,26 +38,111 @@ public class ControllerAddProject implements Initializable {
     @FXML private TableColumn<Project, Integer> columnID;
     @FXML private TableColumn<Project, String> columnTitle;
     @FXML private TableColumn<Project, RelatedCompany> columnCompany;
-    @FXML private TableColumn<Project, ProjectResponsible> columnResponsible;
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        
+    
+    public final void initialize() {
+        linkColumns();
+        enterCorrectInformation();
     }    
 
     @FXML
     private void addCompany() {
+        Stage window = (Stage) tableViewProject.getScene().getWindow();
+        Parent viewFile;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/View_AddCompany.fxml"));
+            viewFile = loader.load();
+            ControllerAddCompany controllerAddCompany = loader.getController();
+            window.setScene(new Scene(viewFile));
+        } catch (IOException exception){
+            Logger.getLogger(ControllerAddProject.class.getName()).log(Level.SEVERE,exception.getMessage(), exception);
+        }
     }
 
     @FXML
     private void registerProject() {
+        if(!areFieldsEmpty()){
+            String title = textFieldTitle.getText();
+            String description = textAreaDescription.getText();
+            String resources = textAreaResources.getText();
+            RelatedCompany company = comboBoxCompany.getValue();
+            
+            Project project = new Project();
+            project.setTitle(title);
+            project.setDescription(description);
+            project.setResources(resources);
+            project.setRequestedBy(company);
+            project.setStatus("Available");
+            if(displayRecordConfirmation()){
+                IProjectDAO projectDAO = new ProjectDAO();
+                if(projectDAO.addElement(project)){
+                    displayRecordSuccessDialog();
+                    cleanFields();
+                } else {
+                    displaySomethingWentWrong();
+                }
+            }
+        } else {
+            displayEmptyFields();
+        }
     }
 
     @FXML
     private void returnProjectSection() {
+        Stage window = (Stage) tableViewProject.getScene().getWindow();
+        Parent viewFile;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/View_ProjectSection.fxml"));
+            viewFile = loader.load();
+            ControllerProjectSection controllerProjectSection = loader.getController();
+            window.setScene(new Scene(viewFile));
+        } catch (IOException exception){
+            Logger.getLogger(ControllerAddProject.class.getName()).log(Level.SEVERE,exception.getMessage(), exception);
+        }
     }
 
     @FXML
-    private void showAllProjects(SortEvent<Project> event) {
+    private void showAllProjects() {
+        linkColumns();
+        IProjectDAO iProject = new ProjectDAO();
+        List<Project> listProject = iProject.getAvailableProjects();
+        if(listProject == null){
+            displaySomethingWentWrong();
+        } else {
+            ObservableList<Project> projectObservableList = FXCollections.observableArrayList(listProject);
+            tableViewProject.setItems(projectObservableList);
+        }
+    }
+    
+    private boolean areFieldsEmpty(){
+        return (textFieldTitle.getText().isEmpty() || textAreaDescription.getText().isEmpty() || 
+                textAreaResources.getText().isEmpty() || comboBoxCompany.getValue() == null);
+    }
+    
+    private void linkColumns(){
+        columnID.setCellValueFactory(new PropertyValueFactory<> ("projectID"));
+        columnTitle.setCellValueFactory(new PropertyValueFactory<> ("title"));
+        columnCompany.setCellValueFactory(new PropertyValueFactory<> ("requestedBy"));
+    }
+    private void cleanFields(){
+        textFieldTitle.setText("");
+        textAreaResources.setText("");
+        textAreaDescription.setText("");
+        comboBoxCompany.setValue(null);
+    }
+    
+    public final void initializeRelatedCompanyComboBox(List<RelatedCompany> companiesRegistered){
+        ObservableList<RelatedCompany> companiesRegisteredOL = FXCollections.observableArrayList(companiesRegistered);
+        comboBoxCompany.getItems().setAll(companiesRegisteredOL);
+    }
+    
+    public void enterCorrectInformation(){
+        textFieldTitle.textProperty().addListener(new ChangeListener<String>(){
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(!newValue.matches("\\sa-zA-Z*")){
+                    textFieldTitle.setText(newValue.replaceAll("[^\\sa-zA-Z]", ""));
+                }
+            }
+        });
     }
 }
